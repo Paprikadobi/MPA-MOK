e9dc39dde5089ba7112912c4b795e26f654a1514
Patrik Dobiáš <patrikdobidobias@gmail.com>
2022-11-25 13:55:21 +0100

    lab8 hw fix, lab9 solution

--- labs/solutions/lab8/hw.py
+++ labs/solutions/lab8/hw.py
@@ -20,25 +20,26 @@ def eval_poly(poly: List[int], x: int, q: int) -> int:
     :param q: Coefficient modulus.
     :return: Evaluation of polynomial.
     """
-    return sum([(coeff * pow(x, i, q)) % q for i, coeff in enumerate(poly)])
+    return sum([(coeff * pow(x, i, q)) % q for i, coeff in enumerate(poly)]) % q
 
 class Tallier:
-    def __init__(self, i: int, t: int, n: int, p: int, g: int):
+    def __init__(self, i: int, t: int, n: int, p: int, q: int, g: int):
         self.i = i
         self.p = p
+        self.q = q
 
-        s = random.randint(0, p - 1)
-        self.poly = gen_poly(s, t - 1, p)
+        self.s = random.randint(0, q - 1)
+        self.poly = gen_poly(self.s, t - 1, q)
 
-        self.hs = [pow(g, s, p) if i == j else 0 for j in range(n)]
+        self.hs = [pow(g, self.s, p) if i == j else 0 for j in range(n)]
         self.shares = [0 for _ in range(n)]
 
     def send_initial_messages(self, talliers: List[Tallier]):
-        self.shares[self.i] = eval_poly(self.poly, self.i, p) % p
+        self.shares[self.i] = eval_poly(self.poly, self.i + 1, self.q)
 
         for tallier in talliers:
             if self.i != tallier.i:
-                tallier.receive_initial_message(self.i, self.hs[self.i], eval_poly(self.poly, tallier.i, p) % p)
+                tallier.receive_initial_message(self.i, self.hs[self.i], eval_poly(self.poly, tallier.i + 1, self.q))
 
     def receive_initial_message(self, i: int, g: int, share: int):
         self.hs[i] = g
@@ -46,10 +47,10 @@ class Tallier:
 
     def finalize_init(self):
         self.h = 1
-        self.x = sum(self.shares)
+        self.x = sum(self.shares) % self.p
 
         for h in self.hs:
-            self.h = (self.h * h) % p
+            self.h = (self.h * h) % self.p
 
     def partial_decrypt(self, c1: int) -> int:
         return pow(c1, self.x, self.p)
@@ -60,13 +61,13 @@ def vote(v: int, p: int, g: int, h: int) -> tuple[int, int]:
     return (pow(g, r, p), (pow(h, r, p) * pow(g, v, p)) % p)
 
 if __name__ == '__main__':
-    p = 11
-    q = 5
-    g = 3
+    p = 29
+    q = 7
+    g = 16
     n = 5
     t = 3
 
-    talliers = [Tallier(i, t, n, p, g) for i in range(n)]
+    talliers = [Tallier(i, t, n, p, q, g) for i in range(n)]
 
     for tallier in talliers:
         tallier.send_initial_messages(talliers)
@@ -84,20 +85,16 @@ if __name__ == '__main__':
         c2 = (c2 * e2) % p
 
     d = 1
-    a = 0
     for tallier in talliers[:t]:
         l = 1
         for j in range(t):
             if tallier.i != j:
-                l = (l * j * pow(j - tallier.i, -1, q)) % q
-
-        a += tallier.x * l
-
+                l = (l * (j + 1) * pow(j - tallier.i, -1, q)) % q
         d = (d * pow(tallier.partial_decrypt(c1), l, p)) % p
 
     m = (c2 * pow(d, -1, p)) % p
 
-    for i in range(n):
+    for i in range(p):
         if pow(g, i, p) == m:
             print(i)
             break
